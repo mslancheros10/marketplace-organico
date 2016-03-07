48cfc01c75210ab5669ae068972d80601f2fac8a
mslancheros10 <ms.lancheros10@uniandes.edu.co>
2016-03-07 10:25:33 -0500

    Provider added to admin

--- main/admin.py
+++ main/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
 
-from main.models import Farm, Product, Basket, ShoppingItem
+from main.models import Farm, Product, Basket, ShoppingItem, Provider
 
 # Register your models here.
+admin.site.register(Provider)
 admin.site.register(Farm)
 admin.site.register(Product)
 admin.site.register(Basket)
