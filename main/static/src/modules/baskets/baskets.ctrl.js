c60f71a960a497c8b5ee69119e74757bcf6ef864
Alejandro Perez <a.perez15@uniandes.edu.co>
2016-03-12 21:19:10 -0500

    add spinner while loading baskets

--- main/static/src/modules/baskets/baskets.ctrl.js
+++ main/static/src/modules/baskets/baskets.ctrl.js
@@ -3,12 +3,16 @@
 
     mod.controller('basketsCtrl', ['$scope', 'basketsService', function ($scope, basketsService) {
 
+        $scope.loading = true;
+
         function responseError(response) {
             console.log(response);
+            $scope.loading = false;
         }
 
         this.getBaskets = function () {
             return basketsService.getBaskets().then(function (response) {
+                $scope.loading = false;
                 console.log(response);
                 $scope.baskets = response.data;
             }, responseError);
