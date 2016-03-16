d63e9d2a70bcdc3b787a035e1a0cf37844d8bbde
Alejandro Perez <a.perez15@uniandes.edu.co>
2016-03-15 23:40:15 -0500

    Pagination

--- main/static/src/modules/products/products.ctrl.js
+++ main/static/src/modules/products/products.ctrl.js
@@ -5,6 +5,23 @@
 
         $scope.loading = true;
 
+        $scope.filteredProducts = [];
+        $scope.currentPage = 1;
+        $scope.numPerPage = 6;
+        $scope.maxSize = 10;
+
+        $scope.$watch('search', function(val) {
+            $scope.filteredProducts = $scope.products
+            $filter('filter')($scope.filteredProducts, val);
+        });
+
+        $scope.$watch('currentPage + numPerPage', function() {
+            var begin = (($scope.currentPage - 1) * $scope.numPerPage);
+            var end = begin + $scope.numPerPage;
+
+            $scope.filteredProducts = $scope.products.slice(begin, end);
+        });
+
         function responseError(response) {
             console.log(response);
             $scope.loading = false;
@@ -15,6 +32,12 @@
                 $scope.loading = false;
                 console.log(response);
                 $scope.products = response.data;
+
+                var begin = (($scope.currentPage - 1) * $scope.numPerPage);
+                var end = begin + $scope.numPerPage;
+
+                $scope.filteredProducts = $scope.products.slice(begin, end);
+
             }, responseError);
         };
 
