615330eb27044c6b603ffe5bda0e08a3260e2e00
catherinevelandia <c.velandia10@uniandes.edu.co>
2016-03-15 23:00:51 -0500

    PO-10: Productores asociados al marketPlace

--- providers/business_logic.py
+++ providers/business_logic.py
@@ -1,64 +1,32 @@
-from main.models import Basket, ShoppingItem
+from main.models import Provider
+from django.contrib.auth.models import User
 
 from django.utils import timezone
 
 '''
-    Convert product to json
+    Transform provider to json format
 '''
-
-
-def shopping_item_to_json(shopping_item):
+def provider_to_json(provider):
     object = {
-        'quantity': shopping_item.quantity,
-        'name': shopping_item.product.name,
-        'price': shopping_item.product.price,
-        'image_url': shopping_item.product.image_url,
-        'unit_value': shopping_item.product.unit_value,
-        'unit_name': shopping_item.product.unit_name,
+        'username': provider.user.username,
+        'firstName': provider.user.first_name,
+        'lastName': provider.user.last_name,
+        'email': provider.user.email,
+        'certificado': provider.certificado
     }
     return object
 
 
-'''
-    Get products from specific basket
-'''
-
-
-def get_basket_products(basket):
-    products = []
-
-    for p in ShoppingItem.objects.filter(basket=basket):
-        products.append(shopping_item_to_json(p))
-
-    return products
 
 
 '''
-    Transform basket to json format
+    Method returning all providers
 '''
+def get_providers_from_model():
 
+    providers = []
 
-def basket_to_json(basket):
-    object = {
-        'name': basket.name,
-        'start_date': basket.start_date,
-        'end_date': basket.end_date,
-        'products': get_basket_products(basket)
-    }
-    return object
+    for p in Provider.objects.all():
+        providers.append(provider_to_json(p))
 
-
-'''
-    Method returning all baskets
-'''
-
-
-def get_baskets_from_model():
-
-    baskets = []
-
-    for b in Basket.objects.filter(start_date__lte=timezone.now(),end_date__gte=timezone.now()):
-        baskets.append(basket_to_json(b))
-
-    return baskets
-
+    return providers
