3a62ced5fe0820e652decb8f69e98bbc98408c91
Alejandro Perez <a.perez15@uniandes.edu.co>
2016-04-09 15:44:33 -0500

    PO-4: (Azul) Test de login

--- session/tests.py
+++ session/tests.py
@@ -1,16 +1,9 @@
-import random
-import string
 from unittest import TestCase
 
 # Create your tests here.
 from selenium import webdriver
-from selenium.webdriver.support.wait import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.common.by import By
-
 
 class SessionFunctionalTest(TestCase):
-
 
     def setUp(self):
         self.browser = webdriver.Firefox()
@@ -20,27 +13,27 @@ class SessionFunctionalTest(TestCase):
 
     def test_login(self):
         self.browser.get('http://localhost:8000')
+        self.login()
+        self.logout()
+
+    def login(self):
         login_option = self.browser.find_element_by_id('id_login')
         login_option.click()
-
         self.browser.implicitly_wait(5)
-
         name = self.browser.find_element_by_id('username')
         name.send_keys('admin')
-
         last_name = self.browser.find_element_by_id('password')
         last_name.send_keys('administrador')
-
         login_button = self.browser.find_element_by_id('id_button_login')
         login_button.click()
 
+    def logout(self):
         self.browser.implicitly_wait(8)
-
         logout_option = self.browser.find_element_by_id('id_option_logout')
 
+        # Check if logout option is enabled
         self.assertIsNotNone(logout_option)
 
         logout_option.click()
-
         self.browser.implicitly_wait(8)
 
