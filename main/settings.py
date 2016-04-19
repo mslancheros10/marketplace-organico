889ebcc00e995b0fd391e3ac91d01143be28f042
mslancheros10 <ms.lancheros10@uniandes.edu.co>
2016-04-18 23:02:12 -0500

    settings modified

--- main/settings.py
+++ main/settings.py
@@ -94,11 +94,11 @@ else:
     DATABASES = {
         'default': {
             'ENGINE': 'django.db.backends.postgresql',
-            'NAME': 'organico',
-            'USER': 'alejo',
-            'PASSWORD': 'alejandro',
-            'HOST': 'organico.cb5016y9yvkl.us-west-2.rds.amazonaws.com',
-            'PORT': '5432',
+            'NAME': os.environ['DATABASE_NAME'],
+            'USER': os.environ['DATABASE_USER'],
+            'PASSWORD': os.environ['DATABASE_PASSWORD'],
+            'HOST': os.environ['DATABASE_HOST'],
+            'PORT': os.environ['DATABASE_PORT'],
         }
     }
 
