5dab6a4e26ecd7cb19ecadf6de7e2dd78a984cc2
mslancheros10 <ms.lancheros10@uniandes.edu.co>
2016-04-19 23:49:23 -0500

    Solucion de conflictos

--- main/static/src/modules/payment/payment.ctrl.js
+++ main/static/src/modules/payment/payment.ctrl.js
@@ -17,7 +17,10 @@
                     $scope.totalandtaxes=0;
                     for (var i = 0; i < $scope.shoppingItems.length; i++) {
                         for (var j = 0; j < $scope.shoppingItems[i].baskets.length; j++) {
-                            $scope.totalandtaxes += $scope.shoppingItems[i].baskets[j].price * $scope.shoppingItems[i].quantity;
+                            for(var k = 0; k< $scope.shoppingItems[i].baskets[j].products.length; k++){
+                                $scope.totalandtaxes += $scope.shoppingItems[i].baskets[j].products[k].price * $scope.shoppingItems[i].baskets[j].products[k].quantity;
+                            }
+
                         }
                         for (var j = 0; j < $scope.shoppingItems[i].products.length; j++) {
                             $scope.totalandtaxes += $scope.shoppingItems[i].products[j].price * $scope.shoppingItems[i].quantity;
@@ -31,83 +34,7 @@
 
 
             };
-            /*$scope.paymentMethod = '';
-            $scope.cardNumber = '';
-            $scope.readonly = false;
-            var hola=getShoppingItems();
-            //var aaa = $scope.shoppingItems[0];
-            $scope.pruebita = "";
 
-            var oldFetch = this.fetchRecords;
-            this.fetchRecords = function () {
-                return oldFetch.call(this).then(function (data) {
-                    self.calcTotal();
-                    return data;
-                });
-            };
-            this.fetchRecords();
-            this.readOnly = true;
-            $scope.lastQuantity = 0;
-            $scope.total = 0;
-            $scope.taxes = 0;
-            this.calcTotal = function () {
-                $scope.total = 0;
-                for (var i = 0; i < $scope.shoppingItems.length; i++) {
-                    $scope.total += $scope.shoppingItems[i].artwork.price * $scope.records[i].quantity;
-                    if ($scope.records[i].artwork.discount)
-                    {
-                        $scope.total += (1 - $scope.records[i].artwork.discount / 100) * $scope.records[i].artwork.price * $scope.records[i].quantity;
-                    } else {
-                        $scope.total += $scope.records[i].artwork.price * $scope.records[i].quantity;
-                    }
-                }
-                $scope.taxes = $scope.total * 0.16;
-                $scope.totalandtaxes = $scope.taxes + $scope.total;
-            };
-            $scope.endPayment = function () {
-                var error = "";
-                var valide = true;
-                var re = "";
-                if ($scope.paymentMethod === '1' || $scope.paymentMethod === '2') {
-                    re = /^(?:4[0-9]{12}(?:[0-9]{3})?)/.exec($scope.cardNumber);
-                    if (re === null) {
-                        valide = false;
-                        error = "Please insert a validad card number. Example: 4512345678912345";
-                    }
-                }
-                if ($scope.paymentMethod === '3') {
-                    re = /[^@]+@[^@]+\.[a-zA-Z]{2,6}/.exec($scope.cardNumber);
-                    if (re === null) {
-                        valide = false;
-                        error = "Please insert a validad email address. Example: example@gmail.com";
-                    }
-                }
-                if (valide) {
-                    paymentSvc.createItem({
-                        method: $scope.paymentMethod,
-                        subtotal: $scope.total,
-                        taxes: $scope.taxes,
-                        total: $scope.totalandtaxes,
-                        reference: $scope.cardNumber
-                    });
-                    for (var i = 0; i < $scope.records.length; i++) {
-                        svc.payItem($scope.records[i]);
-                    }
-                    $scope.paymentMethod = '1';
-                    $scope.cardNumber = '';
-                    $location.path('payf');
-                }
-                else {
-                    $scope.cardNumber = '';
-                    $scope.validationError = "error";
-                    self.showError(error);
-                }
-            };
-            $scope.reset = function () {
-                $scope.paymentMethod = '';
-                $scope.cardNumber = '';
-            };
-       */
          $scope.endPayment = function () {
                 var error = "";
                 var valide = true;
@@ -123,19 +50,7 @@
                     paymentService.svcPayCart({
                                 'price': this.totalpayment
                         });
-                    /*
-                    paymentSvc.createItem({
-                        method: $scope.paymentMethod,
-                        subtotal: $scope.total,
-                        taxes: $scope.taxes,
-                        total: $scope.totalandtaxes,
-                        reference: $scope.cardNumber
-                    });
-                    for (var i = 0; i < $scope.records.length; i++) {
-                        svc.payItem($scope.records[i]);
-                    }
-                    $scope.paymentMethod = '1';
-                    $scope.cardNumber = '';*/
+
                     document.getElementById("paymentModal").style.display="none";
                     document.getElementById("shoppingItemsModal").style.display="none";
                     var fades = document.getElementsByClassName("modal-backdrop fade in");
