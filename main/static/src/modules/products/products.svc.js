dc694b5fb38a0b077a08e65f8a311cb305bfb62e
mslancheros10 <ms.lancheros10@uniandes.edu.co>
2016-05-13 21:25:00 -0500

    Se arreglan bugs del servicio.

--- main/static/src/modules/products/products.svc.js
+++ main/static/src/modules/products/products.svc.js
@@ -1,7 +1,7 @@
 (function (ng) {
     var mod = ng.module('productsModule');
 
-    mod.service('productsService', ['$http', 'productsContext', function ($http, context) {
+    mod.service('productsService', ['$http', 'productsContext', '$location', function ($http, context, $location) {
 
         this.getProducts = function () {
             return $http({
@@ -35,7 +35,16 @@
             return $http({
                 method: 'POST',
                 url: '/registerProductList',
-                data:data
+                data: data
+            }).success(function (response) {
+                if(response=='OK'){
+                    $location.path('main');
+
+                }else{
+                    alert("Debe iniciar sesión como administrador para realizar esta tarea.")
+                }
+            }).error(function (response) {
+                alert("La lista de entrada tiene errores.")
             });
         };
 
